2dd7e6c3d35c974059777d5dd6ec87b5f0855df9
Mitch Reece <mitchellreece18@gmail.com>
2019-06-08 23:50:33 +0100

    Add prettier config options

--- packages/formatters/prettier/index.js
+++ packages/formatters/prettier/index.js
@@ -1,1 +1,13 @@
-module.exports = {};
+module.exports = {
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'es5',
+    overrides: [
+        {
+            files: ['*.json', '*.yml'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
