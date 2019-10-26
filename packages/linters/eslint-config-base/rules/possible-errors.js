89a46a5ed75f2ed35256c9dbb125ec5914e30c12
Mitch Reece <mitchellreece18@gmail.com>
2019-10-26 22:37:17 +0100

    Add rule descriptions to improve readability

--- packages/linters/eslint-config-base/rules/possible-errors.js
+++ packages/linters/eslint-config-base/rules/possible-errors.js
@@ -1,6 +1,9 @@
 module.exports = {
   rules: {
+    // disallow await inside of loops
     'no-await-in-loop': 'error',
+
+    // disallow template literal placeholder syntax in regular strings
     'no-template-curly-in-string': 'error',
   },
 }
--- packages/linters/eslint-config-base/rules/style.js
+++ packages/linters/eslint-config-base/rules/style.js
@@ -1,7 +1,12 @@
 module.exports = {
   rules: {
+    // enforce consistent naming when capturing the current execution context
     'consistent-this': ['error', 'self'],
+
+    // enforce position of line comments
     'line-comment-position': ['error', { position: 'above' }],
+
+    // disallow the unary operators ++ and --
     'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
   },
 }
