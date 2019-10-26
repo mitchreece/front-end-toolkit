caf7e9e0f58a20972b892d6b703f4c7e811eb422
Mitch Reece <mitchellreece18@gmail.com>
2019-10-26 22:06:33 +0100

    Add eslint rules for best practices

--- packages/linters/eslint-config-base/index.js
+++ packages/linters/eslint-config-base/index.js
@@ -1,9 +1,12 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    ...['./rules/variables', './rules/style', './rules/possible-errors'].map(
-      require.resolve
-    ),
+    ...[
+      './rules/variables',
+      './rules/style',
+      './rules/possible-errors',
+      './rules/best-practices',
+    ].map(require.resolve),
   ],
   parserOptions: {
     ecmaVersion: 2019,
---
+++ packages/linters/eslint-config-base/rules/best-practices.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    'require-await': 'error',
+    'default-param-last': 'error',
+    'no-param-reassign': ['error', { props: false }],
+    'no-return-assign': ['error', 'always'],
+    'no-extend-native': 'error',
+    eqeqeq: ['error', 'smart'],
+    yoda: 'error',
+  },
+}
